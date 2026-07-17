4f7cd161f479d34ee6d33fbebcca9d7e6a31e6b7
Martin Kureck <kureckmartin@gmail.com>
2026-07-17 14:10:17 +0200

    Implement Mutex to ensure single instance of the application

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,13 +1,28 @@
 using System;
 using System.Linq;
+using System.Threading;
 using System.Windows;
 
 namespace TeamsStatus
 {
     public partial class App : Application
     {
+        private static Mutex? _mutex = null;
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            const string appName = "TeamsStatusMonitor_SingleInstance_Mutex";
+            
+            _mutex = new Mutex(true, appName, out bool createdNew);
+
+            if (!createdNew)
+            {
+                // App is already running!
+                MessageBox.Show("Teams Status Monitor läuft bereits im Hintergrund (siehe System-Tray).", "Bereits gestartet", MessageBoxButton.OK, MessageBoxImage.Information);
+                Application.Current.Shutdown();
+                return;
+            }
+
             // Prüfen, ob die App mit dem -autostart Argument gestartet wurde
             bool autostart = e.Args.Any(arg => arg.Equals("-autostart", StringComparison.OrdinalIgnoreCase));
             

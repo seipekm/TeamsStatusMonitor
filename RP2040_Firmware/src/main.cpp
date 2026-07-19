70e8293361f4ad5761135bc0396244851f7adf63
Martin Kureck <kureckmartin@gmail.com>
2026-07-19 14:28:28 +0200

    Bump version to 1.2.4 to release bugfix in update parsing

--- RP2040_Firmware/src/main.cpp
+++ RP2040_Firmware/src/main.cpp
@@ -1,7 +1,7 @@
 #include <Arduino.h>
 #include <Adafruit_NeoPixel.h>
 
-#define FIRMWARE_VERSION "1.2.2"
+#define FIRMWARE_VERSION "1.2.4"
 
 // Konfiguration der LED-Matrix
 #define LED_PIN    15   // Data Pin für die WS2812 LEDs
